f10f9cba054e14351f44f284111b75953b9b526e
akmalrusli363 <akmalrusli363@outlook.com>
2020-09-03 19:46:41 +0700

    Initial commit

---
+++ app/src/main/java/com/tilikki/bnccapp/CoronaData.kt
@@ -0,0 +1,11 @@
+package com.tilikki.bnccapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoronaData : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_corona_data)
+    }
+}
