1ffddd480e3d7647be7ce1dc7be7f6cbc77dd72f
akmalrusli363 <akmalrusli363@outlook.com>
2020-09-10 19:05:37 +0700

    Implemented Search, filter, and sorting Lookup Data

--- app/src/main/java/com/tilikki/bnccapp/siagacovid/lookup/LookupActivity.kt
+++ app/src/main/java/com/tilikki/bnccapp/siagacovid/lookup/LookupActivity.kt
@@ -2,28 +2,30 @@ package com.tilikki.bnccapp.siagacovid.lookup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.tilikki.bnccapp.R
 import kotlinx.android.synthetic.main.activity_lookup.*
 
 class LookupActivity : AppCompatActivity() {
-    companion object {
-        const val callLookupActivity = "RETURN_TO_OVERVIEW_ACTIVITY"
-    }
+    private lateinit var mockLookupList : MutableList<LookupData>
 
-    private val mockLookupList = mutableListOf(
-        LookupData("DKI Jakarta", 16538, 1044, 736),
-        LookupData("Jawa Timur", 18308, 9342, 1401),
-        LookupData("Sulawesi Selatan", 8039, 4198, 279),
-        LookupData("Jawa Tengah", 12114, 5244, 464),
-        LookupData("Kalimantan Barat", 3045, 2344, 403),
-        LookupData("Papua", 2133, 1675, 95),
-        LookupData("Bali", 3412, 2313, 101)
-    )
+    fun fetchRegionData() {
+        mockLookupList = mutableListOf(
+            LookupData("${getText(R.string.province_dki_jakarta)}", 16538, 1044, 736),
+            LookupData("${getText(R.string.province_jawa_timur)}", 18308, 9342, 1401),
+            LookupData("${getText(R.string.province_sulawesi_selatan)}", 8039, 4198, 279),
+            LookupData("${getText(R.string.province_jawa_tengah)}", 12114, 5244, 464),
+            LookupData("${getText(R.string.province_kalimantan_barat)}", 3045, 2344, 403),
+            LookupData("${getText(R.string.province_papua)}", 2133, 1675, 95),
+            LookupData("${getText(R.string.province_bali)}", 3412, 2313, 101)
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lookup)
+        fetchRegionData()
 
         val lookupAdapter = LookupAdapter(mockLookupList)
         rvLookUp.layoutManager = LinearLayoutManager(this)
@@ -31,6 +33,14 @@ class LookupActivity : AppCompatActivity() {
 
         ivReturnIcon.setOnClickListener {
             finish()
+        }
+
+        ibClearSearch.setOnClickListener {
+            etRegionLookupSearch.text.clear()
+        }
+
+        etRegionLookupSearch.addTextChangedListener{
+            lookupAdapter.filter.filter(etRegionLookupSearch.text)
         }
     }
 }
--- app/src/main/java/com/tilikki/bnccapp/siagacovid/lookup/LookupAdapter.kt
+++ app/src/main/java/com/tilikki/bnccapp/siagacovid/lookup/LookupAdapter.kt
@@ -2,11 +2,17 @@ package com.tilikki.bnccapp.siagacovid.lookup
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 import com.tilikki.bnccapp.R
+import java.util.*
 
 class LookupAdapter(private val lookupList: MutableList<LookupData>) :
-    RecyclerView.Adapter<LookupViewHolder>() {
+    RecyclerView.Adapter<LookupViewHolder>(), Filterable {
+
+    var filteredLookupList: MutableList<LookupData> = sortByPositivityRate(lookupList)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LookupViewHolder {
         return LookupViewHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.item_lookup, parent, false)
@@ -14,11 +20,40 @@ class LookupAdapter(private val lookupList: MutableList<LookupData>) :
     }
 
     override fun onBindViewHolder(holder: LookupViewHolder, position: Int) {
-        holder.bind(lookupList[position])
+        holder.bind(filteredLookupList[position])
     }
 
     override fun getItemCount(): Int {
-        return lookupList.size
+        return filteredLookupList.size
+    }
+
+    override fun getFilter(): Filter {
+        return object : Filter() {
+            override fun performFiltering(keyword: CharSequence?): FilterResults {
+                val query = keyword.toString()
+                filteredLookupList = if (query.isEmpty()) {
+                    lookupList
+                } else {
+                    lookupList.filter {
+                        it.provinceName.contains(query, true)
+                    } as MutableList<LookupData>
+                }
+                return FilterResults().also {
+                    filteredLookupList = sortByPositivityRate(filteredLookupList)
+                    it.values = filteredLookupList
+                }
+            }
+
+            override fun publishResults(keyword: CharSequence?, result: FilterResults?) {
+                notifyDataSetChanged()
+            }
+        }
+    }
+
+    private fun sortByPositivityRate(list: MutableList<LookupData>): MutableList<LookupData> {
+        return list.sortedByDescending {
+                lookupData: LookupData -> lookupData.numOfPositiveCase
+        } as MutableList<LookupData>
     }
 
 }
