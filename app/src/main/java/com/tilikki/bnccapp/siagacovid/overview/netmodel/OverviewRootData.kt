8ddad03b3d2e5a39c1ef9b82b44a1a83dbdd103c
Akmal <akmalrusli363@outlook.com>
2021-06-24 15:21:14 +0700

    Convert moshi kotlin codegen to kotlin reflection adapter

--- app/src/main/java/com/tilikki/bnccapp/siagacovid/overview/netmodel/OverviewRootData.kt
+++ app/src/main/java/com/tilikki/bnccapp/siagacovid/overview/netmodel/OverviewRootData.kt
@@ -1,9 +1,7 @@
 package com.tilikki.bnccapp.siagacovid.overview.netmodel
 
 import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
 
-@JsonClass(generateAdapter = true)
 data class OverviewRootData(
     @Json(name = "update") val update: OverviewData
 )
