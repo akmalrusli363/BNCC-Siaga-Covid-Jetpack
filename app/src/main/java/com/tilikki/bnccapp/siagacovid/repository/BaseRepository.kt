852955d34a930d1ae33b1e6460fa786e55c8169f
Akmal <akmalrusli363@outlook.com>
2021-06-25 23:48:30 +0700

    Implemented repository base class to reduce boilerplate on declaration

---
+++ app/src/main/java/com/tilikki/bnccapp/siagacovid/repository/BaseRepository.kt
@@ -0,0 +1,25 @@
+package com.tilikki.bnccapp.siagacovid.repository
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.tilikki.bnccapp.siagacovid.utils.jsonadapter.MoshiDateAdapter
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+abstract class BaseRepository {
+    private val moshi = Moshi.Builder()
+        .addLast(KotlinJsonAdapterFactory())
+        .add(MoshiDateAdapter())
+        .build()
+
+    protected val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .baseUrl(getBaseUrl())
+            .build()
+    }
+
+    abstract fun getBaseUrl(): String
+}
--- app/src/main/java/com/tilikki/bnccapp/siagacovid/repository/CovidGovernmentRepositoryImpl.kt
+++ app/src/main/java/com/tilikki/bnccapp/siagacovid/repository/CovidGovernmentRepositoryImpl.kt
@@ -1,25 +1,18 @@
 package com.tilikki.bnccapp.siagacovid.repository
 
-import com.squareup.moshi.Moshi
 import com.tilikki.bnccapp.siagacovid.lookup.netmodel.RegionSummaryData
 import com.tilikki.bnccapp.siagacovid.network.CovidGovernmentApiInterface
 import com.tilikki.bnccapp.siagacovid.network.NetworkConstants
 import com.tilikki.bnccapp.siagacovid.overview.netmodel.OverviewRootData
-import com.tilikki.bnccapp.siagacovid.utils.jsonadapter.MoshiDateAdapter
 import io.reactivex.Observable
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 
-class CovidGovernmentRepositoryImpl : CovidGovernmentRepository {
-    private val moshi = Moshi.Builder().add(MoshiDateAdapter()).build()
-    private val retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-        .baseUrl(NetworkConstants.GOVERNMENT_PROVIDED_COVID19_DATA)
-        .build()
+class CovidGovernmentRepositoryImpl : CovidGovernmentRepository, BaseRepository() {
     private val apiInterface = retrofit.create(CovidGovernmentApiInterface::class.java)
+
+    override fun getBaseUrl(): String {
+        return NetworkConstants.GOVERNMENT_PROVIDED_COVID19_DATA
+    }
 
     override fun getCaseOverview(): Observable<Response<OverviewRootData>> {
         return apiInterface.getCaseOverview()
