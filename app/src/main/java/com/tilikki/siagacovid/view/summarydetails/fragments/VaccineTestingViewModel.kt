b87c8b72d1aa156a9f445184b0dbc25bac74671c
Akmal <akmalrusli363@outlook.com>
2021-08-10 16:52:34 +0700

    Added VaccineTestingViewModel for vaccination & testing history lifecycle

--- app/src/main/java/com/tilikki/siagacovid/view/summarydetails/fragments/VaccineTestingViewModel.kt
+++ app/src/main/java/com/tilikki/siagacovid/view/summarydetails/fragments/VaccineTestingViewModel.kt
@@ -2,12 +2,40 @@ package com.tilikki.siagacovid.view.summarydetails.fragments
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.tilikki.siagacovid.model.CovidTestingData
+import com.tilikki.siagacovid.model.VaccinationOverviewData
+import com.tilikki.siagacovid.repository.CovidGovernmentRepository
+import com.tilikki.siagacovid.repository.CovidGovernmentRepositoryImpl
+import com.tilikki.siagacovid.view.BaseViewModel
 
-class VaccineTestingViewModel : ViewModel() {
+class VaccineTestingViewModel : BaseViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    private var _isFetched: Boolean = false
+    val isFetched: Boolean
+        get() = _isFetched
+
+    private var _tracingHistory: MutableLiveData<List<CovidTestingData>> = MutableLiveData()
+    val tracingHistory: LiveData<List<CovidTestingData>>
+        get() = _tracingHistory
+
+    private var _vaccinationHistory: MutableLiveData<List<VaccinationOverviewData>> = MutableLiveData()
+    val vaccinationHistory: LiveData<List<VaccinationOverviewData>>
+        get() = _vaccinationHistory
+
+    private val governmentRepository: CovidGovernmentRepository = CovidGovernmentRepositoryImpl()
+
+    override fun fetchData() {
+        fetchData(governmentRepository.getTestingVaccinationOverview(), {
+            mapReactiveResponseData(it) { data ->
+                _tracingHistory.postValue(data.testing.history.map { hist ->
+                    hist.toCovidTestingOverviewData()
+                })
+                _vaccinationHistory.postValue(data.vaccination.history.map { hist ->
+                    hist.toVaccinationOverviewData()
+                })
+                _isFetched = true
+            }
+        })
     }
-    val text: LiveData<String> = _text
+
 }
