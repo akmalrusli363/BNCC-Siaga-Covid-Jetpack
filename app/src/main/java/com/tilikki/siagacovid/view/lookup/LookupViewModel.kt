9324d8a343e06f9654a9a09fd22d7e275eab62a9
Akmal <akmalrusli363@outlook.com>
2022-02-11 23:28:30 +0700

    Removed BNPB Covid Repository & regional case data
    As BNPB Covid regional case data are not longer updated, the repository & their data models have been removed

--- app/src/main/java/com/tilikki/siagacovid/view/lookup/LookupViewModel.kt
+++ app/src/main/java/com/tilikki/siagacovid/view/lookup/LookupViewModel.kt
@@ -3,14 +3,12 @@ package com.tilikki.siagacovid.view.lookup
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.tilikki.siagacovid.model.RegionLookupData
-import com.tilikki.siagacovid.repository.BnpbCovidRepository
-import com.tilikki.siagacovid.repository.BnpbCovidRepositoryImpl
 import com.tilikki.siagacovid.repository.CovidGovernmentRepository
 import com.tilikki.siagacovid.repository.CovidGovernmentRepositoryImpl
 import com.tilikki.siagacovid.view.BaseViewModel
 import java.util.*
 
-class LookupViewModel(provideFromGovernment: Boolean) : BaseViewModel() {
+class LookupViewModel() : BaseViewModel() {
     private var _regionData: MutableLiveData<List<RegionLookupData>> = MutableLiveData()
     val regionData: LiveData<List<RegionLookupData>>
         get() = _regionData
@@ -19,27 +17,14 @@ class LookupViewModel(provideFromGovernment: Boolean) : BaseViewModel() {
     val lastUpdated: LiveData<Date?>
         get() = _lastUpdated
 
-    private var _fromGovernment: MutableLiveData<Boolean> = MutableLiveData(provideFromGovernment)
-    val fromGovernment: LiveData<Boolean>
-        get() = _fromGovernment
-
     private var _dataSource: MutableLiveData<String> = MutableLiveData()
     val dataSource: LiveData<String>
         get() = _dataSource
 
     private val covidGovernmentRepo: CovidGovernmentRepository = CovidGovernmentRepositoryImpl()
-    private val bnpbCovidRepo: BnpbCovidRepository = BnpbCovidRepositoryImpl()
 
     override fun fetchData() {
-        if (_fromGovernment.value == false) {
-            fetchAlternativeSource()
-        } else {
-            fetchGovernmentSource()
-        }
-    }
-
-    fun toggleDataSource(fromGovernment: Boolean) {
-        _fromGovernment.postValue(fromGovernment)
+        fetchGovernmentSource()
     }
 
     private fun fetchGovernmentSource() {
@@ -50,18 +35,6 @@ class LookupViewModel(provideFromGovernment: Boolean) : BaseViewModel() {
                     regionData.toRegionLookupData()
                 })
                 _lastUpdated.postValue(data.lastUpdated)
-            }
-        })
-    }
-
-    private fun fetchAlternativeSource() {
-        _dataSource.postValue(bnpbCovidRepo.getDataProviderName())
-        fetchData(bnpbCovidRepo.getRegionalCaseOverview(), {
-            mapReactiveResponseData(it) { data ->
-                _regionData.postValue(data.map { regionData ->
-                    regionData.toRegionLookupData()
-                })
-                _lastUpdated.postValue(null)
             }
         })
     }
