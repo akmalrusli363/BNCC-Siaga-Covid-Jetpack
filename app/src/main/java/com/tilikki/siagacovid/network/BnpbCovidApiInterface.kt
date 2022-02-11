e09d41e80a8698f2820f6b0f877e84f5472a3fe6
Akmal <akmalrusli363@outlook.com>
2022-02-11 22:56:13 +0700

    Deprecate BNPB Covid Repository

--- app/src/main/java/com/tilikki/siagacovid/network/BnpbCovidApiInterface.kt
+++ app/src/main/java/com/tilikki/siagacovid/network/BnpbCovidApiInterface.kt
@@ -5,6 +5,7 @@ import io.reactivex.Observable
 import retrofit2.Response
 import retrofit2.http.GET
 
+@Deprecated("Data provider is not longer updated")
 interface BnpbCovidApiInterface {
     @GET("provinsi")
     fun getRegionalCase(): Observable<Response<List<BnpbRegionalRootData>>>
--- app/src/main/java/com/tilikki/siagacovid/repository/BnpbCovidRepository.kt
+++ app/src/main/java/com/tilikki/siagacovid/repository/BnpbCovidRepository.kt
@@ -4,6 +4,7 @@ import com.tilikki.siagacovid.view.lookup.netmodel.BnpbRegionalRootData
 import io.reactivex.Observable
 import retrofit2.Response
 
+@Deprecated("Data provider is not longer updated")
 interface BnpbCovidRepository : BaseDataRepository {
     fun getRegionalCaseOverview(): Observable<Response<List<BnpbRegionalRootData>>>
 }
